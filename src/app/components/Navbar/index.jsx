ab8827ca5c1e1a4c25921b68daafa12fee24134a
CyberGalactus <edu.garma97@hotmail.com>
2024-01-10 11:06:49 +0100

    index navbar, no style

---
+++ src/app/components/Navbar/index.jsx
@@ -0,0 +1,48 @@
+// Navbar.js
+import Styles from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <nav className={Styles.navbar}>
+            <div className={Styles.leftNav}>
+                <a href="/">
+                    <img
+                        className={Styles.logoImage}
+                        src="/interiorPLus-Svart.png"
+                        alt="Interior plus logo"
+                    />
+                </a>
+            </div>
+
+            <div className={Styles.rightNav}>
+                <ul>
+                    <li>
+                        <a href="/">
+                            <span>
+                                Suppliers
+                            </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="/">
+                            <span>
+                                About us
+                            </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="/">
+                            <span>
+                                Showroom+
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
